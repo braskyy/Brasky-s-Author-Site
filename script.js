a0778aeb333faffef5eacae3a935bf848b6c555e
braskyy <nebraska.pdf@gmail.com>
2026-02-05 11:51:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ const shapesScript = [
 
 let currentSlideIdx = 0;
 // Replace YOUR_SLIDE_ID with your actual Slide ID
-const slideBase = "https://docs.google.com/presentation/d/YOUR_SLIDE_ID/embed?start=false&loop=false&rm=minimal";
+const slideBase = "https://docs.google.com/presentation/d/1PyHlxvv6sGvsd0dFSGJ9S2KjVCDexYPIenSGSET3Mb0/embed?start=false&loop=false&rm=minimal";
 
 function startShapesEgg() {
     const overlay = document.getElementById('shapes-overlay');
